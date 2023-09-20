34f03110b77279bc859f390a2e9d97372a92b395
todendvv91 <todendvv91@gmail.com>
2023-09-20 14:18:35 +0900

    영희가 작업

--- test.js
+++ test.js
@@ -2,8 +2,6 @@ console.log("test1");
 
 // new feature (기능)
 function newFeature(a, b) {
-function abc(a, b) {
-  return a + b;
+  return a * b;
 }
 console.log(newFeature(12, 100));
-console.log(abc(12, 100));
